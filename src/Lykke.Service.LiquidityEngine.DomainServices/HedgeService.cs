4ed442b58c73efcf17bf7dfa44ca5bf64dd15105
Alexey Zelenin <alexey@zelenin.org>
2020-08-25 16:41:59 +0300

    Return MM timer back

--- src/Lykke.Service.LiquidityEngine.DomainServices/HedgeService.cs
+++ src/Lykke.Service.LiquidityEngine.DomainServices/HedgeService.cs
@@ -94,15 +94,14 @@ namespace Lykke.Service.LiquidityEngine.DomainServices
 
                     var finishedAt = DateTime.UtcNow;
 
-                    if (positions.Count != 0)
-                        _log.Info("HedgeService.ExecuteAsync() completed.", new
-                        {
-                            PositionsCount = positions.Count,
-                            TradeIds = positions.Select(x => x.TradeId).ToList(),
-                            StartedAt = startedAt,
-                            FinishedAt = finishedAt,
-                            Latency = (finishedAt - startedAt).TotalMilliseconds
-                        });
+                    _log.Info("HedgeService.ExecuteAsync() completed.", new
+                    {
+                        PositionsCount = positions.Count,
+                        TradeIds = positions.Select(x => x.TradeId).ToList(),
+                        StartedAt = startedAt,
+                        FinishedAt = finishedAt,
+                        Latency = (finishedAt - startedAt).TotalMilliseconds
+                    });
 
                     PrometheusMetrics.HedgeTotalLatency.Inc((finishedAt - startedAt).TotalMilliseconds);
                 }
